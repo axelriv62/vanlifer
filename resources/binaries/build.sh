dbbb03e3911140436b6ff5d1e12350f5adff6dea
Fred Hémery <fred.hemery@univ-artois.fr>
2024-12-13 11:44:26 +0100

    :bug: Modif du nom de la machine

--- resources/binaries/build.sh
+++ resources/binaries/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo "update marathon project "
-rsync -rtv --exclude '.git'  . $NAME@marathon.ipa.iutlens.local:/srv/comptes/marathon24/$NAME/www/
-ssh $NAME@marathon.ipa.iutlens.local "/srv/comptes/marathon24/$NAME/www/resources/binaries/install.sh"
+rsync -rtv --exclude '.git'  . $NAME@172.31.146.106:/srv/comptes/marathon24/$NAME/www/
+ssh $NAME@172.31.146.106 "/srv/comptes/marathon24/$NAME/www/resources/binaries/install.sh"
 
