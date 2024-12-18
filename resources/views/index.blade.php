9a9144f8a821db5416421042667886bbf9002681
Loup Olivier <loup.olivier@gmail.com>
2024-12-18 23:47:47 +0100

    Integration index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,50 @@
     @vite(['resources/css/index.css'])
 @endsection
 @section('content')
-    <h1>Welcome to VanLife</h1>
-    <p>Your adventure starts here.</p>
+    <section class="banner">
+        <div class="content">
+            <img src="{{asset('storage/images/vanlife.webp')}}" alt="bannière">
+            <div class="text">
+                <h1>Bienvenue sur En aVan !</h1>
+                <p>L'aventure commence ici</p>
+            </div>
+        </div>
+    </section>
+    <section class="presentation">
+        <div class="left">
+            <h1>Qui sommes-nous ?</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur eget risus eu urna suscipit ultrices. Maecenas ante nunc, aliquet eget sagittis vitae, egestas nec diam.
+                Cras vel ipsum aliquam, imperdiet ante id, finibus sapien. Suspendisse vel urna a erat fringilla imperdiet quis eget quam.
+                Maecenas sit amet sapien mauris. Suspendisse semper mollis tellus, sed lobortis tortor. Curabitur sit amet condimentum risus. </p>
+        </div>
+        <div class="right">
+
+            <img src="https://baconmockup.com/300/300/" alt="Nous">
+        </div>
+    </section>
+    <section class="voyages">
+        <div class="image">
+            <img src="https://picsum.photos/seed/picsum/300/300" alt="">
+            <div class="text">
+                <p>Voyage en Italie</p>
+            </div>
+        </div>
+        <div class="image">
+            <img src="https://picsum.photos/300/300?grayscale" alt="">
+            <div class="text">
+                <p>Voyage en Italie</p>
+            </div>
+        </div>
+        <div class="image">
+            <img src="https://picsum.photos/300/300/" alt="">
+            <div class="text">
+                <p>Voyage en Italie</p>
+            </div>
+        </div>
+    </section>
+
+
+
 @endsection
 
