2a397e8d93ad8e1dad53cb4fe6167185fd4ad1ec
quentin_tripognez <quentin_tripognez@ens.univ-artois.fr>
2024-12-18 13:59:35 +0100

    :sparkles: Ajout du composant du pied de page.

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,18 @@
+<div class="footer">
+    <div class="footer-gauche">
+        <p href="">Politique de confidentialité</p>
+        <p>Copyright PowerRangers 2024</p>
+    </div>
+    <div class="footer-centre">
+        <img src="{{asset('storage/images/logo.jpg')}}" alt="Logo">
+    </div>
+    <div class="footer-droite">
+        <h4 href="">Restez en contact avec nous</h4>
+        <h8 href="">sur les réseaux sociaux ou par mail</h8>
+        <div class="footer-social">
+            <img src="{{asset('storage/images/logo/instagram-logo.png')}}" alt="Insta">
+            <img src="{{asset('storage/images/logo/youtube-logo.png')}}" alt="Youtube">
+            <img src="{{asset('storage/images/logo/facebook-logo.png')}}" alt="Facebook">
+        </div>
+    </div>
+</div>
