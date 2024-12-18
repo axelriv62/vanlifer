4633908ce2fe01064f365be49e6dacc7a3be3dcb
Loup Olivier <loup.olivier@gmail.com>
2024-12-18 14:59:33 +0100

    Armonisation de méthode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,10 @@
-<x-app-layout>
+@extends('templates.app-layout')
+
+@section('style')
+    @vite(['resources/css/index.css'])
+@endsection
+
     <h1>Welcome to VanLife</h1>
     <p>Your adventure starts here.</p>
-</x-app-layout>
+
+
