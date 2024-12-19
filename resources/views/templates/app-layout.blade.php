5d1b8a38eafc67684f1036a534536ce30ef1a046
Loup Olivier <loup.olivier@gmail.com>
2024-12-19 10:28:24 +0100

    Integration des likes dans voyage

--- resources/views/templates/app-layout.blade.php
+++ resources/views/templates/app-layout.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
 
     <!-- par défaut on charge ces css/js -->
