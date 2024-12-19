3e322274c0ee35aa375471d091f15be09700500f
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-12-19 17:02:59 +0100

    :memo: Mise à jour du vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js',
-                'resources/css/404.css', 'resources/css/index.css', 'resources/css/voyage.css', 'resources/css/voyages.css',
+                'resources/css/404.css', 'resources/css/index.css', 'resources/css/voyage.css', 'resources/css/voyages.css', 'resources/images/team.jpg',
                 'resources/css/test-vite.css', 'resources/js/test-vite.js', "resources/css/login.css", "resources/css/etape.css", "resources/css/form.css", "resources/css/users.css"],
             refresh: true,
         }),
